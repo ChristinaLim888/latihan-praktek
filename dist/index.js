77ba0d0d6ebddf856bfbf7074bcd00d9982bbce2
ChristinaLim888 <Christinalim666@gmail.com>
2025-09-01 23:13:26 +0700

    Belajar TS

---
+++ dist/index.js
@@ -0,0 +1,33 @@
+"use strict";
+// function main () {
+//     console.log("Hello World");
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+// let username: string = "John Doe";
+// console.log(`Username: ${username}`);
+// let age: number = 30;
+// console.log(age);
+// console.log(typeof age);
+// console.log("\n");
+// let username; string = "john Doe";
+// console.log(username);
+// console.log(typeof username);
+//2. Type Inference
+let nama = "Qoli";
+console.log(nama);
+console.log("\n");
+let umur = 21;
+console.log(umur);
+console.log("\n");
+console.log("===Real Case===");
+// Real Case:
+let ticketPrice = 100000;
+let eventName = "Concert";
+let availableTickets = True;
+function calculateTotal(price, qty) {
+    return price * qty;
+}
+// let total = calculateTotal(ticketPrice, 2);
+// console.log(`Total price for 2 tickets to ${eventName}: $${total}`);
+// console.log(`Available tickets: ${availableTickets}`);
+//# sourceMappingURL=index.js.map
